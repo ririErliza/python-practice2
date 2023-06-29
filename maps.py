0394bbed594b66d6e9def4b5c3b981a7a03a2863
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-29 13:44:06 +0200

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,12 +1,27 @@
-# map(function, data)
-# newlist = map(aFunction, aData)
-
 
 from random import shuffle
 
 def jumble(word):
     anagram = list(word)
     shuffle(anagram)
+    return ''.join(anagram)
 
-words = ['beetrot', 'carrots', 'potatoes']
+words = ['aranti', 'erbay']
 anagrams = []
+
+# way 1
+# for word in words:
+#     anagrams.append(jumble(word))
+# print(anagrams)
+
+
+# way 2 using map
+
+# map(function, data)
+# newlist = map(aFunction, aData)
+
+print(list(map(jumble,words)))
+
+print([jumble(word) for word in words]) 
+#this work the same as above
+
