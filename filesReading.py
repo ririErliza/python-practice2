c53396adfb61c73818cbfb982218c3c72de52e75
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-30 12:28:37 +0200

    Update filesReading.py

--- filesReading.py
+++ filesReading.py
@@ -4,8 +4,12 @@ for line in lorem_file:
     # print(line)
     print(line.rstrip())
 
+# seek()
+lorem_file.seek(0)  # reset the cursor at the start of the text using seek()
 
 # readlines()
 
 lines = lorem_file.readlines()
 print(lines)  # this will return us a LIST
+# each line in the text will be in the list separated by comma
+
