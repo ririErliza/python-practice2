ef23f57d2e22ee390548faf47a52c9b5876609ab
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-30 11:00:12 +0200

    Update lambdas.py

--- lambdas.py
+++ lambdas.py
@@ -18,6 +18,12 @@ print(x(5, 6, 2))
 # 13
 
 
+# The power of lambda is better shown when you use them 
+# as an anonymous function inside another function.
+
+# Say you have a function definition that takes one 
+# argument, and that argument  will be multiplied with 
+# an unknown number:
 def myfunc(n):
   return lambda a : a * n
 
