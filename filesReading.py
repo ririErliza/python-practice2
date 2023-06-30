7a486528cd8d68d44e8030464b6a76c1cd12caf7
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-06-30 12:33:07 +0200

    Update filesReading.py

--- filesReading.py
+++ filesReading.py
@@ -1,15 +1,25 @@
 lorem_file = open('files/lorem.txt')
 
-# for line in lorem_file:
-#     # print(line)
-#     print(line.rstrip())
+for line in lorem_file:
+    # print(line)
+    print(line.rstrip())
 
-# # seek()
-# lorem_file.seek(0)  # reset the cursor at the start of the text using seek()
+# seek()
+lorem_file.seek(0)  # reset the cursor at the start of the text using seek()
 
-# # readlines()
+# readlines()
 
-# lines = lorem_file.readlines()
-# print(lines)  # this will return us a LIST
-# # each line in the text will be in the list separated by comma
+lines = lorem_file.readlines()
+print(lines)  # this will return us a LIST
+# each line in the text will be in the list separated by comma
 
+lorem_file.close()
+
+#---------------------------------------------
+lorem_file = open('files/lorem.txt')
+# getting 100 characters from the 50th character
+lorem_file.seek(50)
+file_text=lorem_file.read(100)
+print(file_text)
+
+lorem_file.close()
